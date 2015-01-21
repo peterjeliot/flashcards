896dc6d9e878c7a14b903ddcfb5dd8d2b8cf0e20
vveleva <vvveleva@gmail.com>
2015-01-21 08:50:30 -0800

    Keep only files related to flashcards flip

--- app/helpers/flashcards_helper.rb
+++ app/helpers/flashcards_helper.rb
@@ -1,2 +1,25 @@
 module FlashcardsHelper
+
+	def get_prev(flashcard)
+		flash_hash = Flashcard.all.map.with_index.to_a.to_h
+		flash_hash.delete_if {|key,val| key.subject_id != flashcard.subject_id }
+
+		index = flash_hash[flashcard]
+		flash_hash.delete_if {|key,val| val >= index}
+
+		return flashcard if flash_hash.empty?
+		return flash_hash.keys.last;
+	end
+
+	def get_next(flashcard)
+		flash_hash = Flashcard.all.map.with_index.to_a.to_h
+		flash_hash.delete_if {|key,val| key.subject_id != flashcard.subject_id }
+		
+		index = flash_hash[flashcard]
+		flash_hash.delete_if {|key,val| val <= index}
+		
+		return flashcard if flash_hash.empty?
+		return flash_hash.keys.first;
+	end
+
 end
